a6c921747266905009cc2619a6296413835cc80c
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2023-03-14 23:11:38 +0530

    created ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='App-header'>
+          <h2>Todo List Redux App</h2>
+
+          <form >
+           <input type="text" placeholder='Enter a todo' style={{
+            width:350,
+            padding:10,
+            borderRadius:20,
+            border:"none",
+            fontSize:20,
+           }}/>
+           <button type='submit'style={{
+           
+            padding:12,
+            borderRadius:20,
+            fontSize:15,
+            marginLeft:20,
+           }}>Go</button>
+          </form>
+
+          <ul className='allTodos'>
+            <li className='singleTodo'>
+              <span className='todoText'>First Todo</span>
+              <button style={{
+           
+            padding:10,
+            borderRadius:20,
+            border:"1px solid white",
+            color:"white",
+            backgroundColor:"red",
+            marginLeft:20,
+           }}>Delete</button>
+            </li>
+          </ul>
+
+       </div>
     </div>
   );
 }
